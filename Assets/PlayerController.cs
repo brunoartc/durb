5d11b052db779c86df3392e0401517d31e15e81a
brunoartc <brunoartc@yahoo.com>
2020-06-13 19:57:47 -0300

    initial car physics

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(InputManager))]
+public class PlayerController : MonoBehaviour
+{
+    public InputManager im;
+    public List<WheelCollider> throttleWheels;
+    public List<WheelCollider> steeringWheels;
+    public float strenghtCoefficient = 20000f;
+    public float maxTurn = 20f;
+
+    void Start()
+    {
+        im = GetComponent<InputManager>();
+    }
+
+    void FixedUpdate()
+    {
+
+        foreach (WheelCollider wheel in throttleWheels)
+        {
+            wheel.motorTorque = strenghtCoefficient * Time.deltaTime * im.throttle;
+        }
+
+        foreach (WheelCollider wheel in steeringWheels)
+        {
+            wheel.steerAngle = maxTurn * im.steer;
+        }
+    }
+
+
+
+}
+
