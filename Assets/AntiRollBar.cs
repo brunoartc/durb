f251ae31060d34cb7d42c7b4615c202a354a8d17
brunoartc <brunoartc@yahoo.com>
2020-06-13 22:48:17 -0300

    car physics done

---
+++ Assets/AntiRollBar.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+
+public class AntiRollBar : MonoBehaviour //FROM https://gamedev.stackexchange.com/questions/118388/how-to-do-an-anti-sway-bar-for-a-car-in-unity-5
+{
+
+    public WheelCollider WheelL;
+    public WheelCollider WheelR;
+    private Rigidbody carRigidBody;
+    public float AntiRoll = 5000.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void fixedUpdate()
+    {
+
+        WheelHit hit = new WheelHit();
+        float travelL = 1.0f;
+        float travelR = 1.0f;
+
+        var groundedL = WheelL.GetGroundHit(out hit);
+        if (groundedL)
+        {
+            travelL = (-WheelL.transform.InverseTransformPoint(hit.point).y - WheelL.radius) / WheelL.suspensionDistance;
+        }
+            
+
+        var groundedR = WheelR.GetGroundHit(out hit);
+        if (groundedR)
+        {
+            travelR = (-WheelR.transform.InverseTransformPoint(hit.point).y - WheelR.radius) / WheelR.suspensionDistance;
+        }
+            
+
+        var antiRollForce = (travelL - travelR) * AntiRoll;
+
+        if (groundedL)
+        {
+            carRigidBody.AddForceAtPosition(WheelL.transform.up * -antiRollForce,
+                                         WheelL.transform.position);
+        }
+            
+        if (groundedR)
+        {
+            carRigidBody.AddForceAtPosition(WheelR.transform.up * antiRollForce,
+                                         WheelR.transform.position);
+        }
+            
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,13 +8,18 @@ public class PlayerController : MonoBehaviour
 {
     public InputManager im;
     public List<WheelCollider> throttleWheels;
-    public List<WheelCollider> steeringWheels;
+    public List<GameObject> steeringWheels;
+    public List<GameObject> meshes;
     public float strenghtCoefficient = 20000f;
+    public float brakeCoefficient = 2f;
     public float maxTurn = 20f;
+    public Transform CM;
+    public Rigidbody rb;
 
     void Start()
     {
         im = GetComponent<InputManager>();
+        rb.centerOfMass = CM.position;
     }
 
     void FixedUpdate()
@@ -22,12 +27,26 @@ public class PlayerController : MonoBehaviour
 
         foreach (WheelCollider wheel in throttleWheels)
         {
-            wheel.motorTorque = strenghtCoefficient * Time.deltaTime * im.throttle;
+            
+            if (im.brake)
+            {
+                wheel.motorTorque = 0f;
+                wheel.brakeTorque = brakeCoefficient * Time.deltaTime;
+            } else
+            {
+                wheel.motorTorque = strenghtCoefficient * Time.deltaTime * im.throttle;
+            }
         }
 
-        foreach (WheelCollider wheel in steeringWheels)
+        foreach (GameObject wheel in steeringWheels)
         {
-            wheel.steerAngle = maxTurn * im.steer;
+            wheel.GetComponent<WheelCollider>().steerAngle = maxTurn * im.steer;
+            wheel.transform.localEulerAngles = new Vector3(0f, im.steer *maxTurn, 0f);
+        }
+
+        foreach (GameObject mesh in meshes)
+        {
+            mesh.transform.Rotate(rb.velocity.magnitude * (transform.InverseTransformDirection(rb.velocity).z >= 0 ? 1 : -1) / (2 * Mathf.PI * 0.33f), 0f, 0f);
         }
     }
 
