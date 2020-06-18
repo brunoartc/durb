80a8d457ea14142dd64e3e5556f3ad9c4cc44376
brunoartc <brunoartc@yahoo.com>
2020-06-18 20:21:05 -0300

    done mp

---
+++ Assets/Multyplayer/InputManagerMP.cs
@@ -0,0 +1,27 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InputManagerMP : MonoBehaviourPun
+{
+    public float throttle;
+    public float steer;
+
+    public bool brake;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            throttle = Input.GetAxis("Vertical");
+
+            steer = Input.GetAxis("Horizontal");
+
+            brake = Input.GetKey(KeyCode.Space);
+        }
+    }
+            
+}
---
+++ Assets/Multyplayer/PlayerControllerMP.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(InputManagerMP))]
+public class PlayerControllerMP : MonoBehaviour
+{
+    public InputManagerMP im;
+    public List<WheelCollider> throttleWheels;
+    public List<GameObject> steeringWheels;
+    public List<GameObject> meshes;
+    public float strenghtCoefficient = 20000f;
+    public float brakeCoefficient = 2f;
+    public float maxTurn = 20f;
+    public Transform CM;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        im = GetComponent<InputManagerMP>();
+        //rb.centerOfMass = CM.position;
+    }
+
+    void FixedUpdate()
+    {
+
+        foreach (WheelCollider wheel in throttleWheels)
+        {
+
+            if (im.brake)
+            {
+                wheel.motorTorque = 0f;
+                wheel.brakeTorque = brakeCoefficient * Time.deltaTime;
+            }
+            else
+            {
+                wheel.motorTorque = strenghtCoefficient * Time.deltaTime * im.throttle;
+            }
+        }
+
+        foreach (GameObject wheel in steeringWheels)
+        {
+            wheel.GetComponent<WheelCollider>().steerAngle = maxTurn * im.steer;
+            wheel.transform.localEulerAngles = new Vector3(0f, im.steer * maxTurn, 0f);
+        }
+
+        foreach (GameObject mesh in meshes)
+        {
+            mesh.transform.Rotate(rb.velocity.magnitude * (transform.InverseTransformDirection(rb.velocity).z >= 0 ? 1 : -1) / (2 * Mathf.PI * 0.33f), 0f, 0f);
+        }
+    }
+
+
+
+}
+
