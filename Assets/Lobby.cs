16741eee7bedb141114470adf0f21e11cea68b7c
Nicolas <nicolasbs@al.insper.edu.br>
2020-06-19 18:22:25 -0300

    cenario

---
+++ Assets/Lobby.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lobby : MonoBehaviour
+{
+   void OnTriggerEnter(Collider other) {
+       if (other.CompareTag("Player")) {
+           SceneManager.LoadScene("Lobby_game1");
+       }
+   }
+
+}
